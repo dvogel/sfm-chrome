fb49f86ba44db9ef50e57d689731d012ef9779bc
Drew Vogel <dvogel@sunlightfoundation.com>
2012-03-20 17:17:46 -0400

    Styling and markup changes to reflect changes made to the website. Dropped unused code.

--- js/content_script.js
+++ js/content_script.js
@@ -1,11 +1,3 @@
-var parameters = null;
-var results = null;
-
-var PREFIX_LENGTH = 3;
-var SUFFIX_LENGTH = 3;
-var OPEN_TAG = '<i style="background: yellow none;">';
-var CLOSE_TAG = '</i>';
-
 var standardize_quotes = function (text, leftsnglquot, rightsnglquot, leftdblquot, rightdblquot) {
     return text.replace(/[\u2018\u201B]/g, leftsnglquot)
                .replace(/[\u0027\u2019\u201A']/g, rightsnglquot)
@@ -13,109 +5,47 @@ var standardize_quotes = function (text, leftsnglquot, rightsnglquot, leftdblquo
                .replace(/[\u0022\u201D"]/g, rightdblquot);
 };
 
-var eat_chars = function (text, expected) {
-    var chars_eaten = 0;
-    var found = "";
-
-    var textstream = new String(text);
-    var expected_c = expected.substr(0, 1);
-    while ((found != expected) && (textstream.length > 0)) {
-        var c = textstream.substr(0, 1);
-        var textstream = textstream.slice(1);
-
-        if (c == expected_c) {
-            found += c;
-            expected_c = expected.substr(found.length, 1);
-        } else {
-            chars_eaten += 1;
-        }
-    }
-
-    if (found == expected) {
-        return [true, chars_eaten];
-    } else {
-        return [false, found];
-    }
-};
-
-var highlight_match = function (p, match) {
-    /* This function finds the `match` text in the html of the the
-     * `p` element. The boundaries of the text are determined, then
-     * the html corresponding to the text is wrapped in `OPEN_TAG`
-     * and `CLOSE_TAG`
-     */
-    console.log("");
-    console.log("Highlighting", match, " in: ", $(p).html());
-
-    var text_offset = $(p).text().indexOf(match);
-    if (text_offset == -1) {
-        /* Throw an exception to force callers to limit their calls to 
-         * when they know the `match` text is in the `p` paragraph.
-         */
-        throw "highlight_match: given match text not found in given paragraph"
-    }
-
-    var match_length = match.length;
-    var matches = [];
-    while ((matches.length == 0) && (match_length >= PREFIX_LENGTH)) {
-        console.log("Trying match_length of ", match_length);
-        var html = $(p).html();
-        var prefix = match.substr(0, match_length);
-        var html_offset = html.indexOf(prefix);
-        if (html_offset >= 0) {
-            console.log("Found at html_offset", html_offset);
-            var meal = eat_chars(html.slice(html_offset), match);
-            console.log("meal", meal[0], meal[1]);
-            if (meal[0]) {
-                var prelude = html.slice(0, html_offset);
-                var suffix_boundary = html_offset + match.length + meal[1];
-                var matched = html.slice(html_offset, suffix_boundary);
-                var postlude = html.slice(suffix_boundary);
-                console.log("suffix_boundary", suffix_boundary);
-                console.log("prelude", prelude);
-                console.log("matched", matched);
-                console.log("postlude", postlude);
-                $(p).html(prelude +
-                          OPEN_TAG +
-                          matched +
-                          CLOSE_TAG +
-                          postlude);
-                return;
-            }
-        }
-
-        if (match_length <= (PREFIX_LENGTH * 2)) {
-            match_length -= 2;
-        } else {
-            match_length = Math.ceil(match_length / 2);
-        }
-    }
-};
-
 var handleMessage = function (request, sender, response) {
     if (request.method == 'log') {
         console.log(request.message);
 
     } else if (request.method == 'injectIFrame') {
-        $("#churnalism-overlay").remove();
+        var overlay = $("#churnalism-overlay");
+        if (overlay.length == 0) {
+            var mask = $('<div id="churnalism-mask">\r\n</div>');
+            var overlay_frame = $('<iframe id="churnalism-iframe"></iframe>');
+            overlay = $('<div id="churnalism-overlay"><button id="churnalism-close">Close</button></div>');
 
-        var overlay = $('<div id="churnalism-overlay"><button id="churnalism-close">Close</button></div>');
-        var overlay_frame = document.createElement("iframe");
-        $(overlay_frame).attr('id', 'churnalism-iframe');
+            mask.appendTo("body");
+            overlay_frame.appendTo(overlay);
+            overlay.appendTo("body");
 
-        overlay.append(overlay_frame);
-        $("body").append(overlay);
-        $("#churnalism-close").click(function(click){
-            $("#churnalism-overlay").remove();
-        });
+            $("#churnalism-close").click(function(click){
+                $("#churnalism-mask").fadeOut(function(){ $("#churnalism-mask").remove(); });
+                $("#churnalism-overlay").fadeOut(function(){ $("#churnalism-overlay").remove(); });
+            });
 
-        var doc = overlay_frame.contentDocument || overlay_frame.contentWindow.document;
-        doc.open();
-        doc.writeln(request.content);
-        doc.close();
+            var doc = overlay_frame[0].contentDocument || overlay_frame[0].contentWindow.document;
+            doc.open();
+            doc.writeln(request.content);
+            doc.close();
 
-        $("#churnalism-overlay").scroll(prevent_scroll);
-        $("#churnalism-overlay").bind('mousewheel', prevent_scroll);
+            $("#churnalism-mask").scroll(prevent_scroll);
+            $("#churnalism-mask").bind('mousewheel', prevent_scroll);
+            $("#churnalism-overlay").scroll(prevent_scroll);
+            $("#churnalism-overlay").bind('mousewheel', prevent_scroll);
+
+            overlay.fadeIn();
+            mask.fadeIn();
+        } else {
+            $("#churnalism-iframe").remove();
+            var overlay_frame = $('<iframe id="churnalism-iframe"></iframe>');
+            overlay_frame.appendTo(overlay);
+            var doc = overlay_frame[0].contentDocument || overlay_frame[0].contentWindow.document;
+            doc.open();
+            doc.writeln(request.content);
+            doc.close();
+        }
     } else {
         console.log('content_script.js', 'handleMessage', request, sender, response);
     }
